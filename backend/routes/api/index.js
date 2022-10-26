6d7e396a4b0fd62d3289a4b301ea80105285500f
CoryB <Bogert.cory@gmail.com>
2022-10-26 12:55:17 -0500

    getallspots 75% done

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const spotsRouter = require('./spots.js')
 const { restoreUser, requireAuth } = require("../../utils/auth.js");
 
 router.get('/test', requireAuth, (req, res) => {
@@ -15,6 +16,8 @@ router.get('/test', requireAuth, (req, res) => {
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+router.use('/spots', spotsRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,13 +1,21 @@
 const express = require('express');
+
+const { Spot, Review } = require('../../db/models');
+const sequelize = require('sequelize')
+const {setTokenCookie, requireAuth } = require('../../utils/auth')
+
 const router = express.Router();
-const { Spot } = require('../../db/models');
 
-// const router = require("./spots");
-
-
-router.get('/', async (req, res) => {
-    const spots = await Spot.findAll()
+//needs avgRating and previewImage
+router.get('/', async (req, res, next) => {
+    let spots = await Spot.findAll({
+        include:{model:Review, required: true},
+        attributes:{
+            include: [[sequelize.fn('AVG', sequelize.col('stars')), 'averageRating']]
+        }
+    })
     return res.json(spots)
+
 })
 
 module.exports = router;
