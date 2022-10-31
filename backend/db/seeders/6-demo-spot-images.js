305c0a0f796ab24151f5683b85d6f1a9b522dd00
CoryB <Bogert.cory@gmail.com>
2022-10-31 06:41:39 -0500

    fixed long seeder conflict

--- backend/db/seeders/6-demo-spot-images.js
+++ backend/db/seeders/6-demo-spot-images.js
@@ -15,7 +15,7 @@ module.exports = {
       },
       {
         spotId: 3,
-        url: 'https://www.google.com/search?q=kansas+city+houses&tbm=isch&ved=2ahUKEwi_jdraof76AhWFs2oFHZtDBXQQ2-cCegQIABAA&oq=kansas+city+houses&gs_lcp=CgNpbWcQAzIFCAAQgAQyBAgAEEMyBQgAEIAEMgYIABAFEB4yBggAEAUQHjIGCAAQCBAeMgYIABAIEB4yBggAEAgQHjIGCAAQCBAeMgYIABAIEB46BwgAEIAEEBg6CwgAEIAEELEDEIMBOggIABCABBCxAzoICAAQsQMQgwE6BwgAELEDEEM6CggAELEDEIMBEENQtwdYmhlgqxpoAHAAeACAAYwBiAGcC5IBBDE3LjKYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=VFlZY7-TMoXnqtsPm4eVoAc&bih=1934&biw=1152&rlz=1C1CHBF_enUS922US922',
+        url: 'IaMaUrLLLL.com',
         preview: true
       }
     ])
