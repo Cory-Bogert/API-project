540fc4ea3b5429f2ddd46a499f0736287db7770f
CoryB <Bogert.cory@gmail.com>
2022-11-18 15:55:58 -0600

    fixing everything

---
+++ frontend/src/components/SignupFormModal/SignupForm.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import * as sessionActions from "../../store/session";
+import './SignupForm.css';
+
+
+function SignupFormPage() {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [firstName,setFirstName] = useState("");
+  const [lastName,setLastName] = useState("")
+   if (sessionUser) return <Redirect to="/" />;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password === confirmPassword) {
+      setErrors([]);
+      return dispatch(sessionActions.signup({ email, username, password,firstName,lastName}))
+        .catch(async (res) => {
+          const data = await res.json();
+          if (data && data.errors) setErrors(data.errors);
+        });
+    }
+    return setErrors(['Confirm Password field must be the same as the Password field']);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="error">
+        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+      </div>
+        <h1>Welcome to AirBnb</h1>
+
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+         />
+
+        <input
+          type="text"
+          placeholder="First Name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          required
+        />
+
+      <input
+          type="text"
+          placeholder="Last Name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          required
+        />
+
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        />
+
+      <button className="submit" type="submit">Sign Up</button>
+    </form>
+  );
+}
+
+export default SignupFormPage;
---
+++ frontend/src/store/SpotsReducer.js
@@ -0,0 +1,142 @@
+import { csrfFetch } from "./csrf";
+
+//action types
+const READ_SPOTS = 'spots/READ'
+const READ_SINGLE_SPOT = 'spots/READ_SINGLE_SPOT'
+const CREATE = 'spots/CREATE'
+const DELETE = 'spots/DELETE'
+// const UPDATE = 'spots/UPDATE'
+
+
+//action creators
+const getAll = spots => ({
+    type: READ_SPOTS,
+    spots
+})
+
+const getOne = spot => ({
+    type: READ_SINGLE_SPOT,
+    spot
+})
+
+const create = spot => ({
+    type: CREATE,
+    spot
+})
+
+const remove = spotId => ({
+    type: DELETE,
+    spotId
+})
+
+//thunks
+// read all spots
+export const getAllSpots = () => async dispatch => {
+    const response = await fetch(`/api/spots`);
+    if(response.ok){
+        const spotsList = await response.json()
+        dispatch(getAll(spotsList))
+    }
+}
+
+//read one spot
+export const getOneSpot = (spotId) => async dispatch => {
+    const response = await fetch(`/api/spots/${spotId}`)
+    if(response.ok){
+        const spot = await response.json()
+        dispatch(getOne(spot))
+    }
+}
+
+//create spot
+export const createSpot = (imageload, payload) => async dispatch => {
+    const response = await csrfFetch(`/api/spots`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    });
+        const spot = await response.json();
+        const image = await csrfFetch(`/api/spots/${spot.id}/images`,{
+            method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(imageload)
+        })
+
+    if(response.ok && image.ok ){
+        dispatch(addSpot(spot));
+         return spot
+    }
+
+}
+
+//update
+export const updateSpot = payload => async dispatch => {
+    const response = await csrfFetch(`/api/spots/${payload.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    })
+
+    if(response.ok) {
+        const spot = await response.json()
+        dispatch(create(spot))
+        return spot
+    }
+}
+
+//delete
+export const deleteSpot = spotId => async dispatch => {
+    const response = await csrfFetch(`/api/spots/${spotId}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    if(response.ok){
+        dispatch(remove(spotId))
+        return response
+    }
+}
+
+
+//reducer
+const initialState = {}
+
+const spotsReducer = (state = initialState, action) => {
+    switch(action.type){
+        case READ_SPOTS:
+            const allSpots = {}
+            action.spots.Spots.forEach(spot => {
+                allSpots[spot.id] = spot
+            })
+            return {
+                ...state,
+                ...allSpots
+            }
+
+        case READ_SINGLE_SPOT:
+            const oneState = {...state}
+            oneState[action.spot.id] = action.spot
+            return oneState
+
+        case CREATE:
+            const createState = {...state}
+            createState[action.spot.id] = action.spot
+            return createState
+
+        case DELETE:
+            const deleteState = {...state}
+            delete deleteState[action.spotId]
+            return deleteState
+        default:
+            return state
+    }
+}
+
+export default spotsReducer
